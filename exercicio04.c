09c51a4791240a55683c6f919bd38d6e0dc2f5b9
rafael <rafaelscatarino@hotmail.com>
2023-03-18 18:00:35 -0300

    Organizando o diretorio

---
+++ exercicio04.c
@@ -0,0 +1,11 @@
+/*1. Crie um programa capaz de ler os dados de uma matriz quadrada de inteiros. Ao final da leitura o programa deverá imprimir o núme;ro da linha que contém o menor dentre todos os números lidos.*/
+
+#include <stdio.h>
+
+int main() {
+  int matriz[3] = {1, 2, 3};
+
+  printf(matriz);
+
+  return 0;
+}
