c13077af63965cabc6a4b90b07501472a1379ef6
rafael <rafaelscatarino@hotmail.com>
2023-05-12 12:01:39 -0300

    adicionando tamanhoArrayTipoChar

--- curso_Faculdade/11arrayDeString.c
+++ curso_Faculdade/11arrayDeString.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
   /*Na linguagem C não temos o tipo string como tipos primitivos. Por isso precisamos usar um array de char para guardar um palavra ou uma fraze.*/
-    char palavra[20];
+  char palavra[20];
 
   /*sprintf guarda dentro do array a palavra que você passa como parametro, e ao final da palavra ele coloca um /0 para informar que a palavra acabou...(array: palavra e parametro: MELANCIA).*/
-    sprintf(palavra, "MELANCIA"); 
-    
-    /*O %s faz com que não precise fazer o for passando indece por indice para imprimir uma palavra*/
-    printf("%s", palavra);
-  }
+  sprintf(palavra, "MELANCIA");
+
+  /*O %s faz com que não precise fazer o for passando indece por indice para imprimir uma palavra*/
+  printf("%s", palavra);
+}
--- jogoDaForca.c
+++ jogoDaForca.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-    char palavraSecreta[20];
+int main()
+{
+  char palavraSecreta[20];
 
-    sprintf(palavraSecreta, "MELANCIA|0"); 
-    
-    printf("%s", palavraSecreta);
-  }
+  sprintf(palavraSecreta, "MELANCIA");
+
+  int acertou = 0;
+  int enforcou = 0;
+
+  do {
+    char chute;
+    printf("Informe uma letra: ");
+    scanf ("%c", &chute);
+
+    /*A função strlen se encontra demtro da biiblioteca <string.h>. A função retorna o tamanho de array do tipo CHAR passado como parametro.*/
+    for(int i = 0; i < strlen(palavraSecreta); i++) {
+      if (palavraSecreta[i] == chute) {
+        printf("acertei");
+      }
+    }
+
+  } while(!acertou && !enforcou);
+}
