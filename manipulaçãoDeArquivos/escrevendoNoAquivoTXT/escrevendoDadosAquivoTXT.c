a7e3370dcafaa44c55b7f7c801c644d75b8f6f52
Rafael Catarino <rafaelscatarino@hotmail.com>
2023-06-01 12:02:55 -0300

    adicionando o exercicio outraFormaDeLerUmArquivoTXT

--- manipulaçãoDeArquivos/escrevendoNoAquivoTXT/escrevendoDadosAquivoTXT.c
+++ manipulaçãoDeArquivos/escrevendoNoAquivoTXT/escrevendoDadosAquivoTXT.c
@@ -3,8 +3,8 @@
 int main()
 {	
 	FILE*file;
-	file = fopen("C:\\vscode\\C\\manipulaçãoDeArquivos\\file.txt","w"); /*cria um arquivo .txt no diretorio que foi passado o caminho.*/
-	/*file = fopen("file.txt","w"); cria um arquivo txt na mesma pasta do programa.*/	
+	/*file = fopen("C:\\vscode\\C\\manipulaçãoDeArquivos\\file.txt","w"); cria um arquivo .txt no diretorio que foi passado o caminho.*/
+	file = fopen("file.txt","w"); /*cria um arquivo txt na mesma pasta do programa.*/	
 	fprintf(file,"Meu nome é Rafael.");
 	fclose(file);
 	return 0;
